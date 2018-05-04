10c51e4180ffdb178de424455af6a8fa59d22844
ivc63 <38752519+ivc63@users.noreply.github.com>
2018-05-04 10:16:47 +0300

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,4 +1,5 @@
 @echo off
+mode con: lines=50
 %SystemRoot%\System32\WindowsPowerShell\v1.0\PowerShell.exe Set-ExecutionPolicy Unrestricted
 %SystemRoot%\System32\WindowsPowerShell\v1.0\PowerShell.exe $wc = New-Object System.Net.WebClient; $wc.Encoding = [System.Text.Encoding]::UTF8; iex($wc.DownloadString('https://raw.githubusercontent.com/ivc63/vpn/master/check.ps1'))
 pause
